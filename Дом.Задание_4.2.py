863150886a214e1d685b995329e91ef8ca1c912d
Lorenzo Santi <pajiosta@outlook.com>
2024-10-08 15:57:40 +0300

    Delete Дом.Задание_4.2.py

--- Дом.Задание_4.2.py
+++
@@ -1,7 +0,0 @@
-s = input("Введите строчку: ")
-for i in range(len(s)):
-    if s[i] == "{" or s[i] == "[" or s[i] == "(":
-        x = i
-    elif s[i] == "}" or s[i] == "]" or s[i] == ")":
-        y = i
-print(s[x+1:y])
